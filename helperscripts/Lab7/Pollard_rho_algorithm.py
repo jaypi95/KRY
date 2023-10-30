d1b004c06bd04ec031f968aa3f5ad85107a7f0d5
peterju1 <peterju1@students.zhaw.ch>
2023-10-30 16:22:47 +0100

    seems to work idk if it is correct

---
+++ helperscripts/Lab7/Pollard_rho_algorithm.py
@@ -0,0 +1,16 @@
+from math import gcd
+
+n = 59153
+x0 = 24712
+y0 = x0
+a = 1
+i = 1
+
+while True:
+    x0 = (x0**2 + a) % n
+    y0 = ((y0**2 + a)**2 + a) % n
+    d = gcd(abs(x0 - y0), n)
+
+    if(d > 1 and d < n):
+        print("d = ", d)
+        break
